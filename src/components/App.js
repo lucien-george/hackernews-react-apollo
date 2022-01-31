8b8225f38dadaf693a8239cacadace233e30a6e6
Lucien George <lucien.george@shopify.com>
2022-01-31 11:31:21 +0000

    loading data, creating links, routing, authentication, mutations and store updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
+import LinkList from './LinkList'
+import CreateLink from './CreateLink';
+import Header from './Header';
+import Login from './Login';
+import { Route, Routes } from 'react-router-dom';
 import logo from './../logo.svg';
 import './../styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Routes>
+          <Route exact path="/" element={<LinkList />} />
+          <Route exact path="/create" element={<CreateLink />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </div>
     </div>
   );
 }
